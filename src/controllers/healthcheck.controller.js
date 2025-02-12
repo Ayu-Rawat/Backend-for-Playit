03546b9daefee230434da82620462a0ab147d06f
Ayush rawat <165577927+Ayu-Rawat@users.noreply.github.com>
2025-02-12 12:45:46 +0530

    Update healthcheck response message for testing purposes

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -5,7 +5,7 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const healthcheck = asyncHandler(async (req, res) => {
     try {
-        res.status(200).json(new ApiResponse(200, "OK", "Healthcheck is working fine!"))
+        res.status(200).json(new ApiResponse(200, "OK", "Healthcheck is working fine! checking 1 2 3"))
     } catch (error) {
         throw new ApiError(500, "Internal Server Error", error.message)
     }
